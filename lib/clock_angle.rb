75a7351ea5c6bdb4c0cc00fec6e212fffc4d6bd9
Ian MacDonald & David Abel <student@epicodus.com>
2015-04-20 16:47:40 -0700

    clock angle subtracts hours from minutes converted to degrees

--- lib/clock_angle.rb
+++ lib/clock_angle.rb
@@ -2,8 +2,20 @@ class String
   define_method(:clock_angle) do
     hours_minutes = []
     hours_minutes = self.split(":")
+    angle = 0
     hours_minutes.map!() do |index|
       index.to_i()
     end
+    hours_minutes[0] = hours_minutes[0].*(30)
+    hours_minutes[1] = hours_minutes[1].*(6)
+    if hours_minutes[0].eql?(360)
+      hours_minutes[0] = 0
+    end
+    if hours_minutes[0].>(hours_minutes[1])
+      angle = hours_minutes[0].-(hours_minutes[1])
+    elsif hours_minutes[1].>(hours_minutes[0])
+      angle = hours_minutes[1].-(hours_minutes[0])
+    end
+    angle
   end
 end
